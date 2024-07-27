9b71c39f25fd7d91cb8617aa4bbe60c04bcd33b6
Dragjon <140328303+Dragjon@users.noreply.github.com>
2024-07-27 18:30:48 +0800

    Start

---
+++ Chess-Challenge.Uci/Program.cs
@@ -0,0 +1,26 @@
+﻿using ChessChallenge.Application;
+
+namespace Chess_Challenge.Cli
+{
+    internal class Program
+    {
+        static (int, int) GetTokenCount()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "src", "My Bot", "MyBot.cs");
+            string txt = File.ReadAllText(path);
+            return TokenCounter.CountTokens(txt);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sebastian Lague's Chess Challenge UCI interface by Gediminas Masaitis");
+            var (totalTokens, debugTokens) = GetTokenCount();
+            var tokensWithoutDebug = totalTokens - debugTokens;
+            Console.WriteLine($"Current token count: {tokensWithoutDebug} ({totalTokens} total, {debugTokens} debug)");
+            Console.WriteLine();
+
+            var uci = new Uci();
+            uci.Run();
+        }
+    }
+}
---
+++ Chess-Challenge.Uci/bin/Debug/net6.0/src/My Bot/MyBot.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using ChessChallenge.API;
+public class MyBot : IChessBot
+{
+
+    static readonly int inputLayerSize = 384;
+    static readonly int hiddenLayerSize = 32;
+
+    static float[,] FeatureWeights = new float[inputLayerSize, hiddenLayerSize];
+    static float[] FeatureBias = new float[hiddenLayerSize];
+    static float[] OutputWeights = new float[hiddenLayerSize];
+    static float OutputBias;
+
+    public static int SetWeights()
+    {
+        byte[] iweightsbytes = File.ReadAllBytes("./iweights.bin");
+        byte[] ibiasesbytes = File.ReadAllBytes("./ibiases.bin");
+        byte[] oweightsbytes = File.ReadAllBytes("./oweights.bin");
+        byte[] obiasesbytes = File.ReadAllBytes("./obiases.bin");
+
+        int row = 0;
+        int col = 0;
+
+        // Input weights
+        for (int i = 0; i < inputLayerSize * hiddenLayerSize * 4; i += 4)
+        {
+            byte[] tmp1 = new byte[] { iweightsbytes[i], iweightsbytes[i + 1], iweightsbytes[i + 2], iweightsbytes[i + 3] };
+            FeatureWeights[row, col] = BitConverter.ToSingle(tmp1, 0);
+
+            col++;
+            if (col + 1 == hiddenLayerSize)
+            {
+                col = 0;
+                row++;
+            }
+        }
+
+        // Input biases
+        col = 0;
+        for (int i = 0; i < hiddenLayerSize * 4; i += 4)
+        {
+            byte[] tmp2 = new byte[] { ibiasesbytes[i], ibiasesbytes[i + 1], ibiasesbytes[i + 2], ibiasesbytes[i + 3] };
+            FeatureBias[col] = BitConverter.ToSingle(tmp2, 0);
+            col++;
+        }
+
+        // Output weights
+        col = 0;
+        for (int i = 0; i < hiddenLayerSize * 4; i += 4)
+        {
+            byte[] tmp3 = new byte[] { oweightsbytes[i], oweightsbytes[i + 1], oweightsbytes[i + 2], oweightsbytes[i + 3] };
+            OutputWeights[col] = BitConverter.ToSingle(tmp3, 0);
+            col++;
+        }
+
+        // Output bias
+        byte[] tmp4 = new byte[] { obiasesbytes[0], obiasesbytes[1], obiasesbytes[2], obiasesbytes[3] };
+        OutputBias = BitConverter.ToSingle(tmp4, 0);
+
+        return 0;
+    }
+
+    public Move Think(Board board, Timer timer)
+    {
+        SetWeights();
+        Console.WriteLine($"Feature Weights Length: {FeatureWeights.Length}");
+        Console.WriteLine($"Feature Bias Length: {FeatureBias.Length}");
+        Console.WriteLine($"Output Weights Length: {OutputWeights.Length}");
+        Move[] moves = board.GetLegalMoves();
+        return moves[0];
+    }
+}
