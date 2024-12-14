7464f58cd5e6b6e93de27cad437c296832774115
Dragjon <140328303+Dragjon@users.noreply.github.com>
2024-12-14 21:55:16 +0800

    Update build

--- Chess-Challenge.Uci/obj/Release/net6.0/Chess-Challenge.Cli.AssemblyInfo.cs
+++ Chess-Challenge.Uci/obj/Release/net6.0/Chess-Challenge.Cli.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chess-Challenge.Cli")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ad8211758cac60828632416c4813f10704ae65c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de99817e156a96f91b20d80251f936077bf2a1a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chess-Challenge.Cli")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chess-Challenge.Cli")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
